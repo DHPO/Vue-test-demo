f94c5cf163ecc9c76c8e552189e2fa49a104081e
Jimmy Wang <wangziming1022@foxmail.com>
2020-07-17 15:23:55 +0800

    add e2e test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,24 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains("h1", "计算器");
+  });
+});
+
+describe("Calculator", () => {
+  it("calculate", () => {
+    cy.visit("/");
+
+    cy.get("#input").type("1+2");
+    cy.get("#calculate-btn").click();
+
+    cy.get(".el-table").contains("3");
+
+    cy.get("#input")
+      .clear()
+      .type("bad");
+    cy.get("#calculate-btn").click();
+
+    cy.get(".el-table").contains("Error");
   });
 });
